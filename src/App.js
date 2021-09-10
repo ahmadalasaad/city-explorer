c715798cb25aaeaebf331b00d6be9d84201e4185
ahmadalasaad <ahmadalserese9754@gnail.com>
2021-09-10 03:15:42 +0300

    mmm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 import CityForm from "./components/CityForm"
 import CityList from "./components/CityList";
 import axios from "axios";
@@ -6,16 +6,16 @@ import Alert from 'react-bootstrap/Alert'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CityMovie from "./components/CityMovie";
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      locationData:{},
-      locationName:'',
-      showMap:false,
-      showAlert:false,
-      errMessage:'',
-      WeatherForcast:[],
-      MoviesList:[]
+    this.state = {
+      locationData: {},
+      locationName: '',
+      showMap: false,
+      showAlert: false,
+      errMessage: '',
+      WeatherForcast: [],
+      MoviesList: []
     }
   }
   // handelLocationNameChange=(e)=>{
@@ -28,55 +28,56 @@ class App extends Component {
   handelSubmit = async (e) => {
 
     e.preventDefault();
-    try{
-    const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${e.target.city.value}&format=json`;
-    
-    const response = await axios.get(url);
-       await this.setState({
-      locationData: response.data[0],
-      showMap:true,
-      showAlert:false,
-      errMessage:'',
-    });
-        const serverUrl=`${process.env.REACT_APP_SERVER_URL}/weather?lon=${this.state.locationData.lon}&lat=${this.state.locationData.lat}`;
+    try {
+      const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${e.target.city.value}&format=json`;
+
+      const response = await axios.get(url);
+      await this.setState({
+        locationData: response.data[0],
+        showMap: true,
+        showAlert: false,
+        errMessage: '',
+      });
+      const serverUrl = `${process.env.REACT_APP_SERVER_URL}/weather?lon=${this.state.locationData.lon}&lat=${this.state.locationData.lat}`;
       const serverResponse = await axios.get(serverUrl);
 
-      const serverUrlMov=`${process.env.REACT_APP_SERVER_URL}/movies?query=${e.target.city.value}`;
+      const serverUrlMov = `${process.env.REACT_APP_SERVER_URL}/movies?query=${e.target.city.value}`;
       const serverResponseMov = await axios.get(serverUrlMov);
 
-      this.setState({    
-        WeatherForcast:serverResponse.data,
-        MoviesList:serverResponseMov.data
+      await this.setState({
+        WeatherForcast: serverResponse.data,
+        MoviesList: serverResponseMov.data
       })
       console.log(this.state.MoviesList);
-      
-      
-  } 
-  catch(err) { 
-    this.setState({
-      showMap:false,
-      showAlert:true,
-      errMessage:err.message
-    })}
-  }
-  
 
-render(){
-    return(
+
+    }
+    catch (err) {
+      this.setState({
+        showMap: false,
+        showAlert: true,
+        errMessage: err.message
+      })
+    }
+  }
+
+
+  render() {
+    return (
       <div>
         {
           this.state.showAlert &&
-          <Alert  variant={'danger'}>
-    {this.state.errMessage}
-  </Alert>
+          <Alert variant={'danger'}>
+            {this.state.errMessage}
+          </Alert>
         }
-      <CityForm handelLocationNameChange={this.handelLocationNameChange} handelSubmit={this.handelSubmit}/>
-       {
-        this.state.showMap &&
-      <CityList locationData={this.state.locationData} WeatherForcast={this.state.WeatherForcast} /> 
-    }
-    <CityMovie MoviesList={this.state.MoviesList}/>
-     </div>
+        <CityForm handelLocationNameChange={this.handelLocationNameChange} handelSubmit={this.handelSubmit} />
+        {
+          this.state.showMap &&
+          <CityList locationData={this.state.locationData} WeatherForcast={this.state.WeatherForcast} />
+        }
+        <CityMovie MoviesList={this.state.MoviesList} />
+      </div>
     )
   }
 }
